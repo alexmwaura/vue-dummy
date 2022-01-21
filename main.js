24e5bd6e3de4091bce6856ed2ed7ecce5d23a5ac
alexmwaura <alexmwaura43@gmail.com>
2022-01-21 12:10:33 +0300

    add methods

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
- var app = new Vue ({
+var app = new Vue({
     el: '#app',
     data: {
         product: 'Socks',
-        image : './assets/vmSocks-green-onWhite.jpeg',
-        inventory: 100
+        image: './assets/vmSocks-green-onWhite.jpeg',
+        inventory: 100,
+        details: ["80% cotton", "20% polyster", "Gender-neutral"],
+        variants: [
+            { variantId: 2234, variantColor: 'green', variantImage: './assets/vmSocks-green-onWhite.jpeg' },
+            { variantId: 2235, variantColor: 'blue', variantImage: './assets/vmSocks-blue-onWhite.jpeg ' }
+        ],
+        cart: 0
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateProduct(variantImage) {
+            this.image = variantImage
+        }
     }
 })
